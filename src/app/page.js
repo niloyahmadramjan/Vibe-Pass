bb5a73a87b6b97e2884b340bba956fc499c29333
niloyahmadramjan <niloyahmadramjan@gmail.com>
2025-09-15 17:05:09 +0800

    setup full project for working

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,15 @@
 import React from 'react'
 
 export default function home() {
-  return <div>home</div>
+  return (
+    <section className="bg-bgLight min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gradient">🎬 Vibe Pass</h1>
+        <p className="text-textDark mt-2">
+          Book your movie tickets anytime, anywhere!
+        </p>
+        <button className="btn-primary mt-4">Get Tickets</button>
+      </div>
+    </section>
+  )
 }
